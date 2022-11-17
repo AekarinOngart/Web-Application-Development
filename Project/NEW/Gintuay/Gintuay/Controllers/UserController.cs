e594694d863372987078ad3e25db4bbcefabe2c4
Mon Aekarin <Aekarin.Ongart@gmail.com>
2022-11-18 04:09:15 +0700

    Add files via upload

---
+++ Project/NEW/Gintuay/Gintuay/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Gintuay.Models;
+using Gintuay.Data;
+
+namespace Gintuay.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public UserController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // Create/Edit
+        [HttpPost]
+        public JsonResult CreateEdit(User user)
+        {
+            if(user.Id == 0)
+            {
+                _context.Users.Add(user);
+            } else
+            {
+                var userInDb = _context.Users.Find(user.Id);
+                if (userInDb == null)
+                    return new JsonResult(NotFound());
+
+                userInDb = user;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(user));
+        }
+
+        // Get
+        [HttpGet]
+        public JsonResult Get(int id, string mon)
+        {
+            var result = _context.Users.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+
+        // Delete
+        [HttpDelete]
+        public JsonResult Delete(int id) 
+        {
+            var result = _context.Users.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            _context.Users.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(NoContent());
+        }
+
+        [HttpGet()]
+        public JsonResult GetAll()
+        {
+            var result = _context.Users.ToList();
+
+            return new JsonResult(Ok(result));
+        }
+    }
+}
---
+++ Project/NEW/Gintuay/Gintuay/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Gintuay.Models;
+
+
+namespace Gintuay.Data
+   
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { 
+        
+        }
+    }
+}
